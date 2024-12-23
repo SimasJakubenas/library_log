e3bec23d7aa092539ce0332e0b9a744c1db293df
sjakubenas <sjakubenas@gmail.com>
2024-12-23 13:29:03 +0200

    Break up larger functions into smaller ones for readability

--- modules/main_menu.py
+++ modules/main_menu.py
@@ -9,6 +9,7 @@ from views.main_menu import main_menu_view
 from views.adding_book import add_book_view
 from views.update_book import update_book_view
 from utility.clear import clear
+from utility.split import split_list
 from constants import *
 
 
@@ -24,35 +25,7 @@ def main_menu_controls(user, library_initiation, line_position):
         elif sub_choice == "2":
             clear()
             if isinstance(user, Admin): # Show overdue books as Admin
-                while True:
-                    with open(USER_LOG_PATH, "rb") as pickle_in:
-                        all_users = pickle.load(pickle_in)
-
-                    only_users = list(filter(lambda x: not isinstance(x, Admin), all_users))
-                    users_with_overdue = {}
-                    
-                    for user_instance in only_users:
-                        overdue_books = [book.title for book in user_instance.borrowed_books if book.is_overdue]
-                        users_with_overdue[user_instance.username] = overdue_books
-                    
-                    print("\n" + "-" * 80)
-                    print(" " * 28 + "Users with overdue books")
-                    print("-" * 80 + "\n")
-                    
-                    print(json.dumps(users_with_overdue, indent=4))
-
-                    return_to_menu = input("\nPress ENTER to return to main menu:\n>>> ")
-                    
-                    if return_to_menu == "":
-                        clear()
-                        line_position.empty_line = True
-                        main_menu_view(user, line_position)
-                        break
-                    
-                    else:
-                        clear()
-                        line_position.empty_line = True
-                        print(f"{Bcolors.FAIL}Invalid choice!{Bcolors.ENDC}")
+                show_overdue_admin_view(user,line_position)
      
             else: # Show my books as user
                 line_position.empty_line = True
@@ -64,19 +37,8 @@ def main_menu_controls(user, library_initiation, line_position):
             line_position.empty_line = True
             clear()
             
-            book_title = title_validation(line_position)
-            author = author_validation(line_position)
-            publication_year = publication_year_validation(line_position)
-            genre = genre_validation(line_position)
-            quantity = quantity_validation(line_position)
-                
-            book = Book(book_title, author, publication_year, genre, quantity)
-            library_initiation.add_book(book)
-            
-            line_position.empty_line = False
-            print(f"{Bcolors.OKGREEN}'{book.title}' added to library{Bcolors.ENDC}")
+            add_new_book(line_position, library_initiation)
             main_menu_view(user, line_position)
-            
                 
         elif sub_choice == "0":
             clear()
@@ -223,19 +185,6 @@ def quantity_validation(line_position):
             continue
 
 
-def split_list(lst: list, interval: int):
-    """Creates a list of nested lists which length is based on the interval
-
-    Args:
-        lst (list): any list
-        interval (int): any positive digit that is less than the length of lst
-
-    Returns:
-        list: nested lists of lists
-    """
-    return [lst[i:i + interval] for i in range(0, len(lst), interval)]
-
-
 def book_log_menu(line_position, number_of_pages):
     print("-" * 80)
     print(f"\nPage {line_position.current_page+1}/{number_of_pages}\n")
@@ -308,60 +257,21 @@ def book_log_controls(user, page_choice, number_of_pages, line_position, temp_bo
 def book_log_pagination(user, line_position, book_title=None):
     while True:
         if book_title != None:
-            with open(BOOK_LOG_PATH, 'rb') as pickle_in:
-                book_list = pickle.load(pickle_in)
-                
-            if book_title.lower() != "all":
-                matching_list = []
-                
-                for book in book_list:
-                    if book_title.lower() in book.title.lower():
-                        matching_list.append(book)
+            book_list = matching_list_creation(line_position, book_title)
 
-                book_list = matching_list
-                if len(matching_list) == 0:
-                    clear()
-                    line_position.empty_line = False
-                    print(f"{Bcolors.FAIL}No matching books found{Bcolors.ENDC}")
-                    continue
+            if book_list == False:
+                continue
         
         else:
             book_list = user.borrowed_books
     
         pagination_split = split_list(book_list, PAGINATION)
         number_of_pages = len(pagination_split)
-        if line_position.empty_line == True:
-            print("")
         
         book_table_top(line_position)
+        temp_book_list = display_borrowed_books(user, line_position, book_title, pagination_split)
         
-        temp_book_list = []
-
-        try:
-            for i, book_instance in enumerate(pagination_split[line_position.current_page]):
-                temp_book_list.append(book_instance)
-                
-                if book_title == None and book_instance.borrow_time != None:
-                    
-                    data = dt.datetime.now()
-                    time_borrowed = book_instance.borrow_time
-                    remaining_time = BORROWING_TIME + time_borrowed - (data - dt.datetime(1970,1,1)).total_seconds()
-                    user.user_update()
-                    
-                    if remaining_time < 0:
-                        book_instance.is_overdue = True
-                        user.user_update()
-                    if book_instance.is_overdue == True:
-                        print(f"{Bcolors.FAIL}{i+1} | {book_instance} x |{Bcolors.ENDC}")
-                    else:
-                        print(f"{i+1} | {book_instance} {round(remaining_time):<2}|")
-
-                else:
-                    print(f"{i+1} | {book_instance} {book_instance.quantity} |")
-        except Exception:
-            clear()
-            print(f"{Bcolors.FAIL}You don't have any books, use search option to add books{Bcolors.ENDC}")
-            line_position.empty_line = False
+        if temp_book_list == False:
             break
         
         if len(pagination_split[line_position.current_page]) < 6:
@@ -406,6 +316,7 @@ def book_search_functionality(user, line_position):
             main_menu_view(user, line_position)
             break
 
+
 def update_book_quantity(user, page_choice, line_position, temp_book_list, book_title):
     while True:
         is_right_selection = update_book_view(user, page_choice, line_position, temp_book_list)
@@ -419,75 +330,17 @@ def update_book_quantity(user, page_choice, line_position, temp_book_list, book_
         if ammend_book_log_choice.lower() == 'q': # Adds book
             
             if isinstance(user, Admin):
-                counter_changer = 1
+                add_book(temp_book_list, page_choice, line_position)
                 
-                if temp_book_list[page_choice].quantity < 8:
-                    update_book_counter(counter_changer, temp_book_list, page_choice, line_position)
-                    
-                else:
-                    clear()
-                    print(f"{Bcolors.FAIL}Cannot add book. Max qquantity is 8{Bcolors.ENDC}")
-                    line_position.empty_line = False
             else:
-                clear()
-                main_menu_view(user, line_position)
-                book_choice = temp_book_list[page_choice]
-                
-                if line_position.my_books_menu == False:
-                    line_position.empty_line = False
-                    updated_book_list = user.borrow_book(book_choice)
-                    
-                    break
-                    
-                else:
-                    line_position.empty_line = False
-                    user.return_book(book_choice)
-                    
-                    break
+                show_book(user, line_position, temp_book_list, page_choice)
+                break
                 
         elif ammend_book_log_choice.lower() == 'w' and isinstance(user, Admin): # Removes book
-            counter_changer = -1
-            
-            if temp_book_list[page_choice].quantity > 0:
-                update_book_counter(counter_changer, temp_book_list, page_choice, line_position)
-                
-            else:
-                clear()
-                print(f"{Bcolors.FAIL}Cannot add book. Max qquantity is 8{Bcolors.ENDC}")
-                line_position.empty_line = False
+            remove_book(temp_book_list, page_choice, line_position)
         
         elif ammend_book_log_choice.lower() == 'e' and isinstance(user, Admin): # Deletes book
-            clear()
-            selected_book = temp_book_list[page_choice]
-            
-            with open(BOOK_LOG_PATH, "rb") as pickle_in:
-                book_list = pickle.load(pickle_in)
-
-            filtered_by_text = list(filter(
-                lambda x: x if x.title != selected_book.title else None,
-                book_list
-                ))
-            
-            if book_title != 'all':
-                filtered_by_text = list(filter(
-                    lambda x: x if book_title.lower() in x.title.lower() else None,
-                    filtered_by_text
-                    ))
-                
-                with open(BOOK_LOG_PATH, "wb") as pickle_out:
-                    clear()
-                    pickle.dump(filtered_by_text, pickle_out)
-                    print(f"{Bcolors.OKGREEN}Book remuved successfully{Bcolors.ENDC}")
-                    line_position.empty_line = False
-                
-                return filtered_by_text
-                
-            
-            with open(BOOK_LOG_PATH, "wb") as pickle_out:
-                clear()
-                pickle.dump(filtered_by_text, pickle_out)
-                print(f"{Bcolors.OKGREEN}Book remuved successfully{Bcolors.ENDC}")
-                line_position.empty_line = False
+            filtered_by_text = delete_book(line_position, page_choice, temp_book_list, book_title)
             
             return filtered_by_text
         
@@ -519,3 +372,183 @@ def update_book_counter(counter_changer, temp_book_list, page_choice, line_posit
         pickle.dump(book_list, pickle_out)
         print(f"{Bcolors.OKGREEN}Book updated successfully{Bcolors.ENDC}")
         line_position.empty_line = False
+
+
+def add_new_book(line_position, library_initiation):
+    book_title = title_validation(line_position)
+    author = author_validation(line_position)
+    publication_year = publication_year_validation(line_position)
+    genre = genre_validation(line_position)
+    quantity = quantity_validation(line_position)
+        
+    book = Book(book_title, author, publication_year, genre, quantity)
+    library_initiation.add_book(book)
+    
+    line_position.empty_line = False
+    print(f"{Bcolors.OKGREEN}'{book.title}' added to library{Bcolors.ENDC}")
+
+
+def show_overdue_admin_view(user,line_position):
+    while True:
+        with open(USER_LOG_PATH, "rb") as pickle_in:
+            all_users = pickle.load(pickle_in)
+
+        only_users = list(filter(lambda x: not isinstance(x, Admin), all_users))
+        users_with_overdue = {}
+        
+        for user_instance in only_users:
+            overdue_books = [book.title for book in user_instance.borrowed_books if book.is_overdue]
+            users_with_overdue[user_instance.username] = overdue_books
+        
+        print("\n" + "-" * 80)
+        print(" " * 28 + "Users with overdue books")
+        print("-" * 80 + "\n")
+        
+        print(json.dumps(users_with_overdue, indent=4))
+
+        return_to_menu = input("\nPress ENTER to return to main menu:\n>>> ")
+        
+        if return_to_menu == "":
+            clear()
+            line_position.empty_line = True
+            main_menu_view(user, line_position)
+            break
+        
+        else:
+            clear()
+            line_position.empty_line = True
+            print(f"{Bcolors.FAIL}Invalid choice!{Bcolors.ENDC}")
+
+
+def display_borrowed_books(user, line_position, book_title, pagination_split):
+    temp_book_list = []
+    
+    try:
+        for i, book_instance in enumerate(pagination_split[line_position.current_page]):
+            temp_book_list.append(book_instance)
+            
+            if book_title == None and book_instance.borrow_time != None:
+                
+                data = dt.datetime.now()
+                time_borrowed = book_instance.borrow_time
+                remaining_time = BORROWING_TIME + time_borrowed - (data - dt.datetime(1970,1,1)).total_seconds()
+                user.user_update()
+                
+                if remaining_time < 0:
+                    book_instance.is_overdue = True
+                    user.user_update()
+                if book_instance.is_overdue == True:
+                    print(f"{Bcolors.FAIL}{i+1} | {book_instance} x |{Bcolors.ENDC}")
+                else:
+                    print(f"{i+1} | {book_instance} {round(remaining_time):<2}|")
+                    
+                return temp_book_list
+
+            else:
+                print(f"{i+1} | {book_instance} {book_instance.quantity} |")
+                
+        return temp_book_list
+                
+    except Exception:
+        clear()
+        print(f"{Bcolors.FAIL}You don't have any books, use search option to add books{Bcolors.ENDC}")
+        line_position.empty_line = False
+        
+        return False
+
+
+def matching_list_creation(line_position, book_title):
+    with open(BOOK_LOG_PATH, 'rb') as pickle_in:
+        book_list = pickle.load(pickle_in)
+            
+        if book_title.lower() != "all":
+            matching_list = []
+            
+            for book in book_list:
+                if book_title.lower() in book.title.lower():
+                    matching_list.append(book)
+            
+            if len(matching_list) == 0:
+                clear()
+                line_position.empty_line = False
+                print(f"{Bcolors.FAIL}No matching books found{Bcolors.ENDC}")
+                
+                return False
+            
+            return matching_list
+        
+        else:
+            return book_list
+
+
+def add_book(temp_book_list, page_choice, line_position):
+    counter_changer = 1
+                
+    if temp_book_list[page_choice].quantity < 8:
+        update_book_counter(counter_changer, temp_book_list, page_choice, line_position)
+        
+    else:
+        clear()
+        print(f"{Bcolors.FAIL}Cannot add book. Max qquantity is 8{Bcolors.ENDC}")
+        line_position.empty_line = False
+
+
+def show_book(user, line_position, temp_book_list, page_choice):
+    clear()
+    main_menu_view(user, line_position)
+    book_choice = temp_book_list[page_choice]
+    
+    if line_position.my_books_menu == False:
+        line_position.empty_line = False
+        updated_book_list = user.borrow_book(book_choice)
+        
+    else:
+        line_position.empty_line = False
+        user.return_book(book_choice)
+
+
+def remove_book(temp_book_list, page_choice, line_position):
+    counter_changer = -1
+            
+    if temp_book_list[page_choice].quantity > 0:
+        update_book_counter(counter_changer, temp_book_list, page_choice, line_position)
+        
+    else:
+        clear()
+        print(f"{Bcolors.FAIL}Cannot add book. Max qquantity is 8{Bcolors.ENDC}")
+        line_position.empty_line = False
+
+
+def delete_book(line_position, page_choice, temp_book_list, book_title):
+    clear()
+    selected_book = temp_book_list[page_choice]
+    
+    with open(BOOK_LOG_PATH, "rb") as pickle_in:
+        book_list = pickle.load(pickle_in)
+
+    filtered_by_text = list(filter(
+        lambda x: x if x.title != selected_book.title else None,
+        book_list
+        ))
+    
+    if book_title != 'all':
+        filtered_by_text = list(filter(
+            lambda x: x if book_title.lower() in x.title.lower() else None,
+            filtered_by_text
+            ))
+        
+        with open(BOOK_LOG_PATH, "wb") as pickle_out:
+            clear()
+            pickle.dump(filtered_by_text, pickle_out)
+            print(f"{Bcolors.OKGREEN}Book remuved successfully{Bcolors.ENDC}")
+            line_position.empty_line = False
+        
+        return filtered_by_text
+        
+    with open(BOOK_LOG_PATH, "wb") as pickle_out:
+        clear()
+        pickle.dump(filtered_by_text, pickle_out)
+        print(f"{Bcolors.OKGREEN}Book remuved successfully{Bcolors.ENDC}")
+        line_position.empty_line = False
+    
+    return filtered_by_text
